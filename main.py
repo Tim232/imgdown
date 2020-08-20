99e2c532b8ebdb0759e84f8a478cc9a57b4fa944
Tim232 <64291996+Tim232@users.noreply.github.com>
2020-08-20 15:39:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-#local
+from urllib.parse import urlparse
+import aiofiles
+import aiohttp
+import asyncio
+
+
+async def downloader(urls):
+    for url in urls:
+        parsed_url = urlparse(url)
+        path = parsed_url.path
+        split_path = path.split("/")
+
+        if len(split_path) <= 2:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as response:
+
+                    f = await aiofiles.open(split_path[1], mode="wb")
+                    await f.write(await response.read())
+                    return await f.close()
+
+
+asyncio.run(downloader([]))
