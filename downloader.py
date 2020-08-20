e7b2bf94b935f056f5b8cd4ceae97df88e35a4fb
Tim232 <64291996+Tim232@users.noreply.github.com>
2020-08-21 08:52:13 +0900

    Add files via upload

--- downloader.py
+++ downloader.py
@@ -1,22 +1,43 @@
-redditclientid = ""
-redditclientsecret = ""
-subreddit = "" #needs to exist
+redditclientid = input("Reddit Client Id : ")
+redditclientsecret = input("Reddit Client Secret : ")
+subreddit = input("Exisiting SubReddit : ")
 
-from urllib.parse import urlparse
-import aiofiles
-import aiohttp
+print("Checking Modules...")
+
+print("Importing Internal Modules...")
 import asyncio
-import praw
 import os
+from urllib.parse import urlparse
+print("Imported Internal Modules!")
 
+print("Importing External Modules...")
+
+try: import aiofiles
+except ModuleNotFoundError: 
+    print("aiofiles not found. Installing...")
+    os.system('python -m pip install -U aiofiles==0.5.0')
+    import aiofiles
+
+try: import aiohttp
+except ModuleNotFoundError: 
+    print("aiohttp not found. Installing...")
+    os.system('python -m pip install -U aiohttp==3.6.2')
+    import aiohttp
+
+try: import praw
+except ModuleNotFoundError: 
+    print("praw not found. Installing...")
+    os.system('python -m pip install -U praw==7.1.0')
+    import praw
+    
+print("Imported External Modules!")
 
 reddit = praw.Reddit(
-    client_id=redditclientid,
-    client_secret=redditclientsecret,
-    user_agent="Random",
+    client_id=redditclientid, client_secret=redditclientsecret, user_agent="Random",
 )
 
-def links(limit=10):
+
+def links(limit=100):
     print(limit)
     hot = reddit.subreddit(subreddit).hot(limit=limit)
     submissions = [x for x in hot]
@@ -48,15 +69,25 @@ async def down(url):
     else:
         return print(path)
 
-if __name__ == "__main__":
-    if not os.path.isdir("~/downloads"): os.mkdir('downloads')
-    limit = int(input("How much ? (Default as 10) : "))
-    list = links(limit)
 
-    error = []
-    for url in list:
-        try:
-            asyncio.run(down(url))
-        except:
-            error.append(url)
-    print(error)
+print("Checking whether 'downloads' folder exists...")
+if not os.path.isdir("~/downloads"):
+    print("Creating 'downloads' folder...")
+    os.mkdir("downloads")
+    print("Created!")
+print("Checked!")
+
+limit = int(input("How much images? (Default as 100) : "))
+list = links(limit)
+
+print("May take a while...")
+
+error = []
+for url in list:
+    try:
+        asyncio.run(down(url))
+    except:
+        error.append(url)
+print(error)
+
+input("Press any key to close this console.")
